03bedb32fb43fb3e9fa93e1789bc87f8c9552558
frixxus <frixxus@gmail.com>
2022-03-13 03:23:26 -0700

    Added Input, TrackList, Button

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import TrackList from '../TrackList/TrackList'
+import './Playlist.css'
 
 export default class Playlist extends Component {
 	render() {
-		return <div>Playlist</div>
+		return (
+			<div class='Playlist'>
+				<input defaultValue='New Playlist' />
+				{/* <!-- Add a TrackList component --> */}
+				<TrackList />
+				<button className='Playlist-save'>SAVE TO SPOTIFY</button>
+			</div>
+		)
 	}
 }
