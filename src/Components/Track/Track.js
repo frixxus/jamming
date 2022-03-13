f418aaefd7cac0bcdd6eca323aa11348b0c91d7e
frixxus <frixxus@gmail.com>
2022-03-13 03:25:39 -0700

    Added elements for track values, props as values

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
+import './Track.css'
 
 export default class Track extends Component {
+	isRemoval
+	renderAction = () => {
+		const choice = this.isRemoval ? '+' : '-'
+		return <button className='Track-action'>{choice}</button>
+	}
+
 	render() {
-		return <div>Track</div>
+		return (
+			<div className='Track'>
+				<div className='Track-information'>
+					<h3>{this.props.name}</h3>
+
+					<p>
+						{this.props.artist} | {this.props.album}
+					</p>
+				</div>
+				{this.renderAction}
+			</div>
+		)
 	}
 }
