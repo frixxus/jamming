53261d33a7f7c80730fe1930dd873142f6e1ba44
frixxus <frixxus@gmail.com>
2022-03-13 03:26:13 -0700

    Mapping searchResults to Tracks, BROKEN

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import Track from '../Track/Track'
+import './TrackList.css'
 
 export default class TrackList extends Component {
 	render() {
-		return <div>TrackList</div>
+		return (
+			<div class='TrackList'>
+				{/* <!-- You will add a map method that renders a set of Track components  --> */}
+				{this.props.searchResults.map((track) => (
+					<Track key={track.id} track={track} />
+				))}
+			</div>
+		)
 	}
 }
