2b38841c06d3a08aa344b6756426f64de35ac7d3
frixxus <frixxus@gmail.com>
2022-03-13 03:23:56 -0700

    Added h2, TrackList.

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import TrackList from '../TrackList/TrackList'
+import './SearchResults.css'
 
 export default class SearchResults extends Component {
 	render() {
-		return <div>SearchResults</div>
+		return (
+			<div className='SearchResults'>
+				<h2>Results</h2>
+				{/* <!-- Add a TrackList component --> */}
+				<TrackList tracks={this.props.searchResults} />
+			</div>
+		)
 	}
 }
