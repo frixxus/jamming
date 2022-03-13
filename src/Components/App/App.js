c7153a80cc9650726f6a890f7d7738e0c17dcf39
frixxus <frixxus@gmail.com>
2022-03-13 03:21:56 -0700

    Change to class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,41 @@
+import React, { Component } from 'react'
+import Playlist from '../Playlist/Playlist'
+import SearchBar from '../SearchBar/SearchBar'
+import SearchResults from '../SearchResults/SearchResults'
 import './App.css'
+const track = [
+	{
+		name: 'barbara streisand',
+		artist: 'duck sauce',
+		album: 'justajoke',
+		id: 'jk',
+	},
+]
+export default class App extends Component {
+	constructor(props) {
+		super(props)
 
-function App() {
-	return (
-		<div>
-			<h1>
-				Ja<span className='highlight'>mmm</span>ing
-			</h1>
-			<div className='App'>
-				{/* <!-- Add a SearchBar component --> */}
-				<SearchBar />
-				<div className='App-playlist'>
-					{/* <!-- Add a SearchResults component --> */}
-					<SearchResults />
-					{/* <!-- Add a Playlist component --> */}
-					<Playlist />
+		this.state = { SearchResults: track }
+	}
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className='highlight'>mmm</span>ing
+				</h1>
+				<div className='App'>
+					{/* <!-- Add a SearchBar component --> */}
+					<SearchBar />
+					<div className='App-playlist'>
+						{/* <!-- Add a SearchResults component --> */}
+						<SearchResults
+							searchResults={this.state.SearchResults}
+						/>
+						{/* <!-- Add a Playlist component --> */}
+						<Playlist />
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
-
-export default App
