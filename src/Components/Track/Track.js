0f69ce512312db357eac69043b46a147f564440c
frixxus <frixxus@gmail.com>
2022-03-14 01:40:33 -0700

    Track bug chasing

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
-import './Track.css'
+import React, { Component } from 'react';
+import './Track.css';
 
 export default class Track extends Component {
-	isRemoval
-	renderAction = () => {
-		const choice = this.isRemoval ? '+' : '-'
-		return <button className='Track-action'>{choice}</button>
-	}
+	// isRemoval;
+	// renderAction = () => {
+	// 	const choice = this.isRemoval ? '+' : '-';
+	// 	return <button className='Track-action'>{choice}</button>;
+	// };
 
 	render() {
 		return (
 			<div className='Track'>
 				<div className='Track-information'>
-					<h3>{this.props.name}</h3>
+					<h3>{this.props.track.name}</h3>
 
 					<p>
-						{this.props.artist} | {this.props.album}
+						{this.props.track.artist} | {this.props.track.album}
 					</p>
 				</div>
-				{this.renderAction}
+				{/* {this.renderAction} */}
 			</div>
-		)
+		);
 	}
 }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
-import Track from '../Track/Track'
-import './TrackList.css'
+import React, { Component } from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
 
 export default class TrackList extends Component {
 	render() {
+		// const getTracks = this.props.tracks.map((track) => {
+		// 	return <Track key={track.id} track={track} />;
+		// });
 		return (
 			<div class='TrackList'>
 				{/* <!-- You will add a map method that renders a set of Track components  --> */}
-				{/* {this.props.searchResults.map((track) => (
+				{this.props.tracks.map((track) => (
 					<Track key={track.id} track={track} />
-				))} */}
+				))}
+				{/* <Track track={this.props.tracks} /> */}
+				{/* {getTracks} */}
 			</div>
-		)
+		);
 	}
 }
