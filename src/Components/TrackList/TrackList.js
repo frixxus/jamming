c3037884547ed088d034a62011e229956e4a741b
frixxus <frixxus@gmail.com>
2022-03-14 22:13:23 -0700

    Fixed render error, Playlist sendt undefined prop

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 import Track from '../Track/Track'
 import './TrackList.css'
 
-const track = {
-	name: 'barbara streisand',
-	artist: 'duck sauce',
-	album: 'justajoke',
-	id: '1',
-}
 export default class TrackList extends Component {
 	render() {
 		return (
 			<div className='TrackList'>
 				{this.props.tracks.map((track) => (
-					<Track key={track.id} track={track} />
+					<Track
+						key={track.id}
+						track={track}
+						onAdd={this.props.onAdd}
+					/>
 				))}
 			</div>
 		)
