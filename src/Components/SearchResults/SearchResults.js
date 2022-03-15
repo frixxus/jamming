b20b275314f3b38d65d7a64b23a297fceecee91c
frixxus <frixxus@gmail.com>
2022-03-14 22:11:20 -0700

    Added onAdd, isRemoval attributes to Tracklist

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -8,7 +8,11 @@ export default class SearchResults extends Component {
 			<div className='SearchResults'>
 				<h2>Results</h2>
 				{/* <!-- Add a TrackList component --> */}
-				<TrackList tracks={this.props.searchResults} />
+				<TrackList
+					tracks={this.props.searchResults}
+					onAdd={this.props.onAdd}
+					isRemoval={false}
+				/>
 			</div>
 		)
 	}
