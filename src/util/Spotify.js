a9df1cc1507ebf2948f574c7fefbbb66b61c220a
frixxus <frixxus@gmail.com>
2022-03-20 10:56:53 -0700

    Created spotify module

---
+++ src/util/Spotify.js
@@ -0,0 +1,26 @@
+let accessToken
+const clientId = 'cd64a6acff0b420099091a65cd044f11'
+const redirectURI = 'http://localhost:3000/'
+const Spotify = {
+	getAccessToken() {
+		if (accessToken) {
+			return accessToken
+		}
+		const accessTokenMatch =
+			window.location.href.match(/access_token=([^&]*)/)
+		const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+		if (accessTokenMatch && expiresInMatch) {
+			accessToken = accessTokenMatch[1]
+			const expiresIn = expiresInMatch[1]
+
+			window.setTimeout(() => (accessToken = ''), expiresIn * 1000)
+			window.history.pushState('Access Token', null, '/')
+		} else {
+			const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+			window.location(accessURL)
+		}
+	},
+}
+
+export default Spotify
